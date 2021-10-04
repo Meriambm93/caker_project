2066880c83d44b7d2b7cb47fd74cfbfae5e6477e
Meriam B <meriambm0704@gmail.com>
2021-10-04 15:53:46 +0200

    chore: Header

---
+++ caker/src/components/Header.jsx
@@ -0,0 +1,52 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome" // Import the FontAwesomeIcon component
+import { faUser } from "@fortawesome/free-solid-svg-icons" // import the icons you need
+
+const Header = () => {
+  return (
+    <nav className="flex items-center justify-between flex-wrap bg-mygrey p-6">
+      <div className="flex items-center flex-shrink-0 text-mygold mr-6 w-1/3">
+        <span className="ml-10 text-2xl font-medium tracking-tight">
+          <a href="#">LE COIN DES GOURMANDISES</a>
+        </span>
+      </div>
+      <div className="w-1/2 block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div className="text-sm lg:flex-grow px-5">
+          <a
+            href="#responsive-header"
+            className="mt-4 inline-block text-mygold hover:text-mypurple mr-10"
+          >
+            ACCUEIL
+          </a>
+          <a
+            href="#responsive-header"
+            className="mt-4 inline-block text-mygold hover:text-mypurple mr-10"
+          >
+            PROFIL
+          </a>
+          <a
+            href="#responsive-header"
+            className="mt-4 inline-block text-mygold hover:text-mypurple mr-10"
+          >
+            TROUVER MON PATISSIER
+          </a>
+          <a
+            href="#responsive-header"
+            className="mt-4 inline-block text-mygold hover:text-mypurple"
+          >
+            VENDS TES PATISSERIES
+          </a>
+        </div>
+        <div>
+          <a
+            href="#"
+            className="text-xl mt-4 mr-10 inline-block text-black hover:text-mypurple"
+          >
+            <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
+          </a>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
