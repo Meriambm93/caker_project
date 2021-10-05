d53a39bb54b42844c515d76ff879b99938426a55
leawan <leawan@gmail.com>
2021-10-05 12:34:07 +0200

    feat/ patissier/pages

---
+++ caker/src/components/ContainerPatissier.jsx
@@ -0,0 +1,179 @@
+const ContainerPatissier= () => {
+  return (
+    <div className="container py-5">
+      <div className="row">
+        <div className="col-lg-3">
+          <h1 className="h2 pb-4">Pâtissier</h1>
+          <p>
+            DES CRÉATIONS ANCRÉES DANS LA TRADITION ET TOURNÉES VERS LA
+            MODERNITÉ
+          </p>
+        </div>
+
+        <div className="col-lg-9">
+          <div className="row">
+            <div className="col-md-6"></div>
+            <div className="col-md-6 pb-4">
+              <div className="d-flex">
+                <div className="col-md-8 pb-6"></div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="card mb-4 product-wap rounded-0">
+                <div className="card rounded-0">
+                  <img
+                    className="card-img rounded-0 img-fluid"
+                    src="assets/img/patissier/ramirez.png"
+                  />
+                  <div className="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
+                    <ul className="list-unstyled">
+                      <li>
+                        <a
+                          className="btn btn-success text-white mt-2"
+                          href="shop_1.html"
+                        >
+                          <i className="far fa-eye"></i>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="card-body">
+                  <a
+                    href="shop-single.html"
+                    className="h3 text-decoration-none"
+                  >
+                    ramirez
+                  </a>
+                  <ul className="w-100 list-unstyled d-flex justify-content-between mb-0">
+                    <li>Paris</li>
+                    <li className="pt-2">
+                      <span className="product-color-dot color-dot-red float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-blue float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-black float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-light float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-green float-left rounded-circle ml-1"></span>
+                    </li>
+                  </ul>
+                  <ul className="list-unstyled d-flex justify-content-center mb-1">
+                    <li>
+                      <i className="text-warning fa fa-star"></i>
+                      <i className="text-warning fa fa-star"></i>
+                      <i className="text-warning fa fa-star"></i>
+                      <i className="text-muted fa fa-star"></i>
+                      <i className="text-muted fa fa-star"></i>
+                    </li>
+                  </ul>
+                  <p className="text-center mb-0">Day Dreams Cake Designer</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card mb-4 product-wap rounded-0">
+                <div className="card rounded-0">
+                  <img
+                    className="card-img rounded-0 img-fluid"
+                    src="assets/img/patissier/yummy.png"
+                  />
+                  <div className="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
+                    <ul className="list-unstyled">
+                      <li>
+                        <a
+                          className="btn btn-success text-white mt-2"
+                          href="shop_2.html"
+                        >
+                          <i className="far fa-eye"></i>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="card-body">
+                  <a
+                    href="shop-single.html"
+                    className="h3 text-decoration-none"
+                  >
+                    Elisa
+                  </a>
+                  <ul className="w-100 list-unstyled d-flex justify-content-between mb-0">
+                    <li>Paris 19 ème</li>
+                    <li className="pt-2">
+                      <span className="product-color-dot color-dot-red float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-blue float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-black float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-light float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-green float-left rounded-circle ml-1"></span>
+                    </li>
+                  </ul>
+                  <ul className="list-unstyled d-flex justify-content-center mb-1">
+                    <li>
+                      <i className="text-warning fa fa-star"></i>
+                      <i className="text-warning fa fa-star"></i>
+                      <i className="text-warning fa fa-star"></i>
+                      <i className="text-muted fa fa-star"></i>
+                      <i className="text-muted fa fa-star"></i>
+                    </li>
+                  </ul>
+                  <p className="text-center mb-0">Bakery Elisa</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card mb-4 product-wap rounded-0">
+                <div className="card rounded-0">
+                  <img
+                    className="card-img rounded-0 img-fluid"
+                    src="assets/img/patissier/kristen.png"
+                  />
+                  <div className="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
+                    <ul className="list-unstyled">
+                      <li>
+                        <a
+                          className="btn btn-success text-white mt-2"
+                          href="shop_3.html"
+                        >
+                          <i className="far fa-eye"></i>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="card-body">
+                  <a
+                    href="shop-single.html"
+                    className="h3 text-decoration-none"
+                  >
+                    Fatima
+                  </a>
+                  <ul className="w-100 list-unstyled d-flex justify-content-between mb-0">
+                    <li>Noisy-le-sec</li>
+                    <li className="pt-2">
+                      <span className="product-color-dot color-dot-red float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-blue float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-black float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-light float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-green float-left rounded-circle ml-1"></span>
+                    </li>
+                  </ul>
+                  <ul className="list-unstyled d-flex justify-content-center mb-1">
+                    <li>
+                      <i className="text-warning fa fa-star"></i>
+                      <i className="text-warning fa fa-star"></i>
+                      <i className="text-warning fa fa-star"></i>
+                      <i className="text-muted fa fa-star"></i>
+                      <i className="text-muted fa fa-star"></i>
+                    </li>
+                  </ul>
+                  <p className="text-center mb-0">Fatima Cupcakes</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ContainerPatissier;
