e84e9cc3057181a497ac731ee2f7fdaef7bbe152
A.Ab <a.ayariabir@gmail.com>
2021-10-08 14:50:35 +0200

    feat: shop1 pages

---
+++ caker/src/components/Shop1.jsx
@@ -0,0 +1,210 @@
+import Image from "next/image"
+import shopImage from "../assets/images/tarte-framboise.png"
+import shopImage1 from "../assets/images/framboise-lait-amandes.png"
+import shopImage2 from "../assets/images/le-fraise-vanille.png"
+const ContentShop1 = () => {
+  return (
+    <div className="container py-5">
+      <div className="row">
+        <div className="col-lg-3">
+          <br />
+          <h1 className="h2 pb-4">Ramirez</h1>
+          <ul className="list-unstyled templatemo-accordion">
+            <li className="pb-3">
+              <a className="orshop collapsed d-flex justify-content-between h3 text-decoration-none">
+                Voir le profil
+                <i className="fa fa-fw fa-chevron-circle-down mt-1"></i>
+              </a>
+              <ul className="collapse show list-unstyled pl-3">
+                <li>
+                  <p className="profilshop">
+                    Je m'appelle Ramirez, passionné par la pâtisserie je vous
+                    propose mes gâteaux qui sont élaborés à partir de produits
+                    de bonne qualité. n'hésitez pas à me contacter ,toutes les
+                    garnitures et décors peuvent être modifiés sur demande...
+                    alors n'hésitez plus... pas de livraison..... les commandes
+                    sont a récupérer sur noisy-le-sec (93)
+                  </p>
+                </li>
+              </ul>
+            </li>
+            <li className="pb-3">
+              <a className="orshop collapsed d-flex justify-content-between h3 text-decoration-none">
+                Me conctater
+                <i className="pull-right fa fa-fw fa-chevron-circle-down mt-1"></i>
+              </a>
+              <ul id="collapseTwo" className="collapse list-unstyled pl-3">
+                <li>
+                  <p className="profilshop">ramirez@gmail.com</p>
+                </li>
+                <li>
+                  <p className="profilshop">Whatsapp: 06.56.89.09.09</p>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <div className="col-lg-9 ">
+          <div className="row">
+            <div className="col-md-6"></div>
+            <div className="col-md-6 pb-4">
+              <div className="d-flex">
+                <div className="col-md-8 pb-6"></div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="card mb-4 product-wap rounded-0">
+                <div className="card rounded-0">
+                  <Image
+                    className="card-Image rounded-0 Image-fluid"
+                    src={shopImage}
+                    alt="tarte"
+                  />
+                  <div className="card-Image-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
+                    <ul className="list-unstyled">
+                      <li>
+                        <a
+                          className="btn btn-success text-white mt-2"
+                          href="shop-product.html"
+                        >
+                          <i className="fas fa-cart-plus"></i>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="card-body">
+                  <a
+                    href="shop-product.html"
+                    className="h3 text-decoration-none"
+                  >
+                    Tropezienne
+                  </a>
+                  <ul className="w-100 list-unstyled d-flex justify-content-between mb-0">
+                    <li className="pt-2">
+                      <span className="product-color-dot color-dot-red float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-blue float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-black float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-light float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-green float-left rounded-circle ml-1"></span>
+                    </li>
+                  </ul>
+                  <ul className="list-unstyled d-flex justify-content-center mb-1">
+                    <li>
+                      <i className="text-warning fa fa-star"></i>
+                      <i className="text-warning fa fa-star"></i>
+                      <i className="text-warning fa fa-star"></i>
+                      <i className="text-muted fa fa-star"></i>
+                      <i className="text-muted fa fa-star"></i>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card mb-4 product-wap rounded-0">
+                <div className="card rounded-0">
+                  <Image
+                    className="card-Image rounded-0 Image-fluid"
+                    src={shopImage1}
+                    alt="tarte"
+                  />
+                  <div className="card-Image-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
+                    <ul className="list-unstyled">
+                      <li>
+                        <a
+                          className="btn btn-success text-white mt-2"
+                          href="shop-product-1.html"
+                        >
+                          <i className="fas fa-cart-plus"></i>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="card-body">
+                  <a
+                    href="shop-product-1.html"
+                    className="h3 text-decoration-none"
+                  >
+                    Cake amandes litchi
+                  </a>
+                  <ul className="w-100 list-unstyled d-flex justify-content-between mb-0">
+                    <li className="pt-2">
+                      <span className="product-color-dot color-dot-red float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-blue float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-black float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-light float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-green float-left rounded-circle ml-1"></span>
+                    </li>
+                  </ul>
+                  <ul className="list-unstyled d-flex justify-content-center mb-1">
+                    <li>
+                      <i className="text-warning fa fa-star"></i>
+                      <i className="text-warning fa fa-star"></i>
+                      <i className="text-warning fa fa-star"></i>
+                      <i className="text-muted fa fa-star"></i>
+                      <i className="text-muted fa fa-star"></i>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card mb-4 product-wap rounded-0">
+                <div className="card rounded-0">
+                  <Image
+                    className="card-Image rounded-0 Image-fluid"
+                    src={shopImage2}
+                    alt="tarte"
+                  />
+                  <div className="card-Image-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
+                    <ul className="list-unstyled">
+                      <li>
+                        <a
+                          className="btn btn-success text-white mt-2"
+                          href="shop-product-2.html"
+                        >
+                          <i className="fas fa-cart-plus"></i>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="card-body">
+                  <a
+                    href="shop-product-2.html"
+                    className="h3 text-decoration-none"
+                  >
+                    framboise au lait d'amandes
+                  </a>
+                  <ul className="w-100 list-unstyled d-flex justify-content-between mb-0">
+                    <li className="pt-2">
+                      <span className="product-color-dot color-dot-red float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-blue float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-black float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-light float-left rounded-circle ml-1"></span>
+                      <span className="product-color-dot color-dot-green float-left rounded-circle ml-1"></span>
+                    </li>
+                  </ul>
+                  <ul className="list-unstyled d-flex justify-content-center mb-1">
+                    <li>
+                      <i className="text-warning fa fa-star"></i>
+                      <i className="text-warning fa fa-star"></i>
+                      <i className="text-warning fa fa-star"></i>
+                      <i className="text-muted fa fa-star"></i>
+                      <i className="text-muted fa fa-star"></i>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default ContentShop1
--- caker/src/components/SignUp.jsx
+++ caker/src/components/SignUp.jsx
@@ -1,40 +1,21 @@
 import Image from "next/image"
 import cupCake from "../assets/images/cake.jpg"
+import FormInput from "./FormInput"
 
 const ContentSignUp = () => {
   return (
     <div className="wrapper">
       <div className="inner">
         <div className="image-holder">
-          <Image src={cupCake} alt="Image" width={500} height={500} />
+          <Image src={cupCake} alt="Image" className="img" />
         </div>
         <form action="">
           <h3>S'inscrire</h3>
-
-          <div className="form-holder ">
-            <input type="text" placeholder="nom" className="form-control" />
-          </div>
-          <div className="form-holder active">
-            <input type="text" placeholder="prenom" className="form-control" />
-          </div>
-          <div className="form-holder">
-            <input type="text" placeholder="e-mail" className="form-control" />
-          </div>
-          <div className="form-holder">
-            <input
-              type="password"
-              placeholder="mot de passe"
-              className="form-control"
-            />
-          </div>
-          <div className="form-holder">
-            <input
-              type="password"
-              placeholder="confirmer le mot de passe"
-              className="form-control"
-            />
-          </div>
-
+          <FormInput type="text" placeholder="nom" />
+          <FormInput type="text" placeholder="prenom" />
+          <FormInput type="text" placeholder="e-mail" />
+          <FormInput type="password" placeholder="mot de passe" />
+          <FormInput type="password" placeholder="confirme mot de passe" />
           <div className="form-login">
             <button>Envoyer</button>
             <p className="fs-5">
