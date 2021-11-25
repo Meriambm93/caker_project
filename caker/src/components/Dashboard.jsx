c58e777379fa0d1e40d3d0e14f39be74b1a3a599
Meriam B <meriambm0704@gmail.com>
2021-11-25 15:28:51 +0100

    Modification des composants Header Dashbord et Profil

--- caker/src/components/Dashboard.jsx
+++ caker/src/components/Dashboard.jsx
@@ -1,8 +1,143 @@
-import Image from "next/image"
-import tarteFramboise from "../assets/images/tarte-framboise.png"
+import { faHome, faPowerOff, faTable } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const Dashboard = () => {
-  return <div></div>
+  return (
+    <div>
+      <div>
+        <div className="area">
+          <h3>Tableau de bord</h3>
+        </div>
+        <nav className="main-menu">
+          <ul>
+            <li>
+              <a href="">
+                <FontAwesomeIcon icon={faHome} className="iconeDashboard" />
+                <i className="fa fa-home fa-2x"></i>
+                <span className="nav-text">Accueil</span>
+              </a>
+            </li>
+
+            <li>
+              <a href="#">
+                <FontAwesomeIcon icon={faTable} className="iconeDashboard2" />
+                <i className="fa fa-table fa-2x"></i>
+                <span className="nav-text">Tables</span>
+              </a>
+            </li>
+          </ul>
+
+          <ul className="logout">
+            <li>
+              <a href="#">
+                <FontAwesomeIcon
+                  icon={faPowerOff}
+                  className="iconeDashboard3"
+                />
+                <i className="fa fa-power-off fa-2x"></i>
+                <span className="nav-text">Déconnexion</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="fondDashboard">
+        {" "}
+        <div className="tableDashboard">
+          <table className="table">
+            <thead className="thead-dark">
+              <tr className="colorDash">
+                <th scope="col">#</th>
+                <th scope="col">Prénom</th>
+                <th scope="col">Nom</th>
+                <th scope="col">Email</th>
+                <th scope="col">Ajouter</th>
+                <th scope="col">Modifier</th>
+                <th scope="col">Supprimer</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="colorDashb1">
+                <th scope="row">1</th>
+                <td>Mériam</td>
+                <td>MB</td>
+                <td>mb@gmail.com</td>
+                <td>
+                  <button className="dashboardAdd">Ajouter</button>
+                </td>
+                <td>
+                  <button className="dashboardUp">Modifier</button>
+                </td>
+                <td>
+                  <button className="dashboardDelete">Supprimer</button>
+                </td>
+              </tr>
+              <tr className="colorDashb2">
+                <th scope="row">2</th>
+                <td>Abir</td>
+                <td>AA</td>
+                <td>abirA@gmail.com</td>
+                <td>
+                  <button className="dashboardAdd">Ajouter</button>
+                </td>
+                <td>
+                  <button className="dashboardUp">Modifier</button>
+                </td>
+                <td>
+                  <button className="dashboardDelete">Supprimer</button>
+                </td>
+              </tr>
+              <tr className="colorDashb1">
+                <th scope="row">3</th>
+                <td>Youssef</td>
+                <td>BOUG</td>
+                <td>boug@gmail.fr</td>
+                <td>
+                  <button className="dashboardAdd">Ajouter</button>
+                </td>
+                <td>
+                  <button className="dashboardUp">Modifier</button>
+                </td>
+                <td>
+                  <button className="dashboardDelete">Supprimer</button>
+                </td>
+              </tr>
+              <tr className="colorDashb2">
+                <th scope="row">4</th>
+                <td>Ignacio</td>
+                <td>EE</td>
+                <td>ignacioe@gmail.com</td>
+                <td>
+                  <button className="dashboardAdd">Ajouter</button>
+                </td>
+                <td>
+                  <button className="dashboardUp">Modifier</button>
+                </td>
+                <td>
+                  <button className="dashboardDelete">Supprimer</button>
+                </td>
+              </tr>
+              <tr className="colorDashb1">
+                <th scope="row">5</th>
+                <td>Léa</td>
+                <td>wW</td>
+                <td>lea@gmail.fr</td>
+                <td>
+                  <button className="dashboardAdd">Ajouter</button>
+                </td>
+                <td>
+                  <button className="dashboardUp">Modifier</button>
+                </td>
+                <td>
+                  <button className="dashboardDelete">Supprimer</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Dashboard
--- caker/src/components/Profil.jsx
+++ caker/src/components/Profil.jsx
@@ -1,15 +1,63 @@
 import Link from "next/link"
 import Image from "next/image"
 import avatar from "../assets/images/avatar.jpg"
+import FormField from "./FormField"
 import FormInput from "./FormInput"
-import FormLabel from "./FormLabel"
 import Button from "../../src/components/Button"
+import { Formik } from "formik"
+import { useCallback } from "react"
+import * as yup from "yup"
+import axios from "axios"
+import { useRouter } from "next/router"
 
+const validationSchema = yup.object().shape({
+  firstName: yup.string().required().min(1).max(120),
+  lastName: yup.string().required().min(1).max(120),
+  email: yup.string().email().required(),
+  password: yup.string().min(8).required(),
+  address: yup.string().min(1).required(),
+  city: yup.string().min(1).max(120).required(),
+  zipCode: yup.number().integer().min(1).required(),
+  role_id: yup.number().integer().min(1).required(),
+})
 const Profil = () => {
+  const router = useRouter()
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    address: "09 route",
+    city: "Paris",
+    zipCode: 93160,
+    profilePicture: "",
+    role_id: 3,
+  }
+  const handleFormSubmit = useCallback(
+    async (values, actions) => {
+      try {
+        await axios.get("http://localhost:5000/user/profil/1", {
+          firstName: values.firstName,
+          lastName: values.lastName,
+          email: values.email,
+          password: values.password,
+          address: values.address,
+          city: values.city,
+          zipCode: values.zipCode,
+          profilePicture: values.profilePicture,
+          role_id: values.role_id,
+        })
+        router.push("/")
+      } catch (err) {
+        actions.setErrors({ form: "une erreur" })
+      }
+    },
+    [router],
+  )
+
   return (
     <div className="container light-style flex-grow-1 container-p-y ">
       <h4 className="font-weight-bold py-3 mb-4 text-warning"> Mon Profil</h4>
-
       <div className="card overflow-hidden">
         <div className="row no-gutters row-bordered row-border-light">
           <div className="col-md-3 pt-0">
@@ -34,7 +82,7 @@ const Profil = () => {
                 className="tab-pane fade active show p-2"
                 id="account-general"
               >
-                <div className="card-body media align-items-center">
+                <div className="photoProfil card-body media align-items-center">
                   <Image
                     src={avatar}
                     width={90}
@@ -45,86 +93,124 @@ const Profil = () => {
                   <div className="media-body ml-4">
                     <label className="avatar btn btn-outline-primary">
                       modifier la photo profil
-                      <input
+                      <Button
                         type="file"
                         className="account-settings-fileinput bg-transparent borde-0"
                       />
-                    </label>{" "}
-                    &nbsp;
+                    </label>
                   </div>
                 </div>
-                <hr className="border-light m-0" />
-                <div className="card-body bg-light">
-                  <div className="form-group">
-                    <FormLabel>Nom</FormLabel>
-                    <input
-                      type="text"
-                      className="form-control mb-1"
-                      placeholder="amandine"
-                    />
-                  </div>
-                  <div className="form-group">
-                    <FormLabel>Prénom</FormLabel>
-                    <FormInput type="text" placeholder="rose" />
-                  </div>
-                  <div className="form-group">
-                    <FormLabel>E-mail</FormLabel>
-                    <input
-                      type="text"
-                      className="form-control mb-1"
-                      placeholder="patisserie@mail.com"
-                    />
-                  </div>
-                  <div className="form-group">
-                    <FormLabel>Adresse</FormLabel>
-                    <FormInput type="text" placeholder="11 rue du chocolat" />
-                  </div>
-                  <div className="form-group">
-                    <FormLabel>Ville</FormLabel>
-                    <FormInput type="text" placeholder="paris" />
-                  </div>
-                  <div className="form-group">
-                    <FormLabel>Code postal</FormLabel>
-                    <FormInput type="number" placeholder="75006" />
-                  </div>
-                  <div className="form-group">
-                    <FormLabel>Téléphone</FormLabel>
-                    <FormInput type="number" placeholder="0623456789" />
-                  </div>
-                </div>
-                <h1 className="profil"> Modifier Mot de passe</h1>
-                <div
-                  className="tab-pane active show"
-                  id="account-change-password"
+                <Formik
+                  initialValues={initialValues}
+                  onSubmit={handleFormSubmit}
+                  validationSchema={validationSchema}
                 >
-                  <div className="card-body pb-2 bg-light">
-                    <div className="form-group">
-                      <FormLabel>Mot de passe actuel</FormLabel>
-                      <FormInput type="password" />
-                    </div>
-                    <div className="form-group">
-                      <FormLabel>Nouveau Mot de passe</FormLabel>
-                      <FormInput type="password" />
-                    </div>
-
-                    <div className="form-group">
-                      <FormLabel>Répéter le nouveau mot de passe</FormLabel>
-                      <FormInput type="password" />
-                    </div>
-                  </div>
-                </div>
+                  {({ handleSubmit, errors }) =>
+                    console.log(errors) || (
+                      <form onSubmit={handleSubmit} className="form-v100">
+                        <hr className="border-light m-0" />
+                        <div className="card-body bg-light">
+                          <div className="form-group">
+                            <FormField
+                              as={FormInput}
+                              type="text"
+                              className="form-control mb-1"
+                              placeholder="amandine"
+                              name="firstName"
+                            />
+                          </div>
+                          <div className="form-group">
+                            <FormField
+                              as={FormInput}
+                              type="text"
+                              placeholder="rose"
+                              name="lastName"
+                            />
+                          </div>
+                          <div className="form-group">
+                            <FormField
+                              as={FormInput}
+                              type="text"
+                              className="form-control mb-1"
+                              placeholder="patisserie@mail.com"
+                              name="email"
+                            />
+                          </div>
+                          <div className="form-group">
+                            <FormField
+                              as={FormInput}
+                              type="text"
+                              placeholder="11 rue du chocolat"
+                              name="address"
+                            />
+                          </div>
+                          <div className="form-group">
+                            <FormField
+                              as={FormInput}
+                              type="text"
+                              placeholder="paris"
+                              name="city"
+                            />
+                          </div>
+                          <div className="form-group">
+                            <FormField
+                              as={FormInput}
+                              type="number"
+                              pattern="[0-9]{5}"
+                              placeholder="75006"
+                              name="zipCode"
+                              min="1"
+                              max="99999"
+                            />
+                          </div>
+                        </div>
+                        <h1 className="profil"> Modifier Mot de passe</h1>
+                        <div
+                          className="tab-pane active show"
+                          id="account-change-password"
+                        >
+                          <div className="card-body pb-2 bg-light">
+                            <div className="form-group">
+                              <FormField
+                                as={FormInput}
+                                type="password"
+                                placeholder="mot de passe actuel"
+                                name="password"
+                              />
+                            </div>
+                            <div className="form-group">
+                              <FormField
+                                as={FormInput}
+                                type="password"
+                                placeholder="nouveau mot de passe"
+                                name="password2"
+                              />
+                            </div>
+                            <div className="form-group">
+                              <FormField
+                                as={FormInput}
+                                type="password"
+                                placeholder="répeter le nouveau mot de passe"
+                                name="password2"
+                              />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="text-right mt-5 mb-5">
+                          <Link href="/" passHref>
+                            <Button type="button">Sauvegarder</Button>
+                          </Link>
+                          &nbsp;
+                          <Button type="button">Annuler</Button>
+                        </div>
+                      </form>
+                    )
+                  }
+                </Formik>
               </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <div className="text-right mt-5 mb-5">
-        <Link href="/" passHref>
-          <Button type="button">Sauvegarder</Button>
-        </Link>
-        &nbsp;
-        <Button type="button">Annuler</Button>
       </div>
     </div>
   )
